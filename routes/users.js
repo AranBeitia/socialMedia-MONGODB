527b1c7843b32f92a1c291f10a5cb860957ca351
AranBeitia <arantza.beitia@gmail.com>
2023-07-06 20:54:27 +0200

    wip

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,7 @@ router.post(
 	uploadUserImages.single('user_img'),
 	[
 		check('firstName', 'The first name cant be empty').notEmpty(),
-		check('userame', 'The username cant be empty').notEmpty(),
+		check('username', 'The username cant be empty').notEmpty(),
 		check('age', 'The age cant be empty').notEmpty(),
 		check('email', 'The email format is not valid').isEmail(),
 		check('password', 'The password cant be empty').notEmpty(),
