877aa848b07debbdf231325e2ceff0f9692139dd
AranBeitia <arantza.beitia@gmail.com>
2023-07-06 19:47:41 +0200

    update user

--- models/User.js
+++ models/User.js
@@ -1,49 +1,53 @@
-const mongoose = require("mongoose");
-const ObjectId = mongoose.SchemaTypes.ObjectId;
+const mongoose = require('mongoose')
+const ObjectId = mongoose.SchemaTypes.ObjectId
 
 const UserSchema = new mongoose.Schema(
-    {
-        firstName: String,
-        lastName: String,
-        username: {
-            type: String,
-            unique: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-        },
-        password: String,
-        user_img: String,
-        darkMode: Boolean,
-        bio: String,
-        role: String,
-        followers: [
-            {
-                type: ObjectId,
-                ref: "User",
-            },
-        ],
-        following: [
-            {
-                type: ObjectId,
-                ref: "User",
-            },
-        ],
-        confirmed: Boolean,
-        tokens: [],
-        postIds: [{ type: ObjectId, ref: "Post" }],
-    },
-    { timestamps: true }
-);
+	{
+		firstName: String,
+		lastName: String,
+		username: {
+			type: String,
+			unique: true,
+		},
+		email: {
+			type: String,
+			unique: true,
+		},
+		age: {
+			type: Number,
+			required: [true, 'Por favor rellena tu edad'],
+		},
+		password: String,
+		user_img: String,
+		darkMode: Boolean,
+		bio: String,
+		role: String,
+		followers: [
+			{
+				type: ObjectId,
+				ref: 'User',
+			},
+		],
+		following: [
+			{
+				type: ObjectId,
+				ref: 'User',
+			},
+		],
+		confirmed: Boolean,
+		tokens: [],
+		postIds: [{ type: ObjectId, ref: 'Post' }],
+	},
+	{ timestamps: true }
+)
 
 UserSchema.methods.toJSON = function () {
-    const user = this._doc;
-    delete user.tokens;
-    delete user.password;
-    return user;
-};
+	const user = this._doc
+	delete user.tokens
+	delete user.password
+	return user
+}
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,49 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controllers/UserController");
-const { authentication } = require("../middlewares/authentication");
-const validateBodyParams = require("../middlewares/validateBodyParams");
-const { check } = require("express-validator");
-const { uploadUserImages } = require("../middlewares/upload");
+const express = require('express')
+const router = express.Router()
+const UserController = require('../controllers/UserController')
+const { authentication } = require('../middlewares/authentication')
+const validateBodyParams = require('../middlewares/validateBodyParams')
+const { check } = require('express-validator')
+const { uploadUserImages } = require('../middlewares/upload')
 
-router.get("/", UserController.getAllUsers);
-router.get("/getLoggedUser", authentication, UserController.getLoggeduser);
-router.get("/id/:_id", UserController.getUserById);
-router.get("/getUserByUsername/:username", UserController.getUserByUsername);
-router.get("/getUsersQuery", UserController.getUsersQuery);
-router.get("/confirm/:email", UserController.confirm);
+router.get('/', UserController.getAllUsers)
+router.get('/getLoggedUser', authentication, UserController.getLoggeduser)
+router.get('/id/:_id', UserController.getUserById)
+router.get('/getUserByUsername/:username', UserController.getUserByUsername)
+router.get('/getUsersQuery', UserController.getUsersQuery)
+router.get('/confirm/:email', UserController.confirm)
 
 router.post(
-    "/",
-    uploadUserImages.single("user_img"),
-    [
-        check("firstName", "The first name cant be empty").notEmpty(),
-        check("username", "The username cant be empty").notEmpty(),
-        check("email", "The email format is not valid").isEmail(),
-        check("password", "The password cant be empty").notEmpty(),
-        check("password2", "Please, repeat the password").notEmpty(),
-        validateBodyParams,
-    ],
-    UserController.createUser
-);
-router.post("/login", UserController.login);
-router.post("/logout", authentication, UserController.logout);
+	'/register',
+	uploadUserImages.single('user_img'),
+	[
+		check('firstName', 'The first name cant be empty').notEmpty(),
+		check('userame', 'The username cant be empty').notEmpty(),
+		check('age', 'The age cant be empty').notEmpty(),
+		check('email', 'The email format is not valid').isEmail(),
+		check('password', 'The password cant be empty').notEmpty(),
+		check('password2', 'Please, repeat the password').notEmpty(),
+		validateBodyParams,
+	],
+	UserController.createUser
+)
+router.post('/login', UserController.login)
+router.delete('/logout', authentication, UserController.logout)
 
 router.put(
-    "/updateLoggedUser",
-    authentication,
-    uploadUserImages.single("user_img"),
-    [
-        check("firstName", "The first name cant be empty").notEmpty(),
-        check("username", "The username cant be empty").notEmpty(),
-        validateBodyParams,
-    ],
-    UserController.updateLoggedUser
-);
-router.put("/follow/:_id", authentication, UserController.follow);
-router.put("/unfollow/:_id", authentication, UserController.unfollow);
+	'/updateLoggedUser',
+	authentication,
+	uploadUserImages.single('user_img'),
+	[
+		check('firstName', 'The first name cant be empty').notEmpty(),
+		check('username', 'The username cant be empty').notEmpty(),
+		validateBodyParams,
+	],
+	UserController.updateLoggedUser
+)
+router.put('/follow/:_id', authentication, UserController.follow)
+router.put('/unfollow/:_id', authentication, UserController.unfollow)
 
-router.delete("/:_id", UserController.deleteUser);
+router.delete('/:_id', UserController.deleteUser)
 
-module.exports = router;
+module.exports = router
