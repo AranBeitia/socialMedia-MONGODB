cf568e8355966eea92fe1e438c2ca2e00b904244
AranBeitia <arantza.beitia@gmail.com>
2023-07-09 22:20:47 +0200

    wip

--- routes/posts.js
+++ routes/posts.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const router = express.Router();
-const PostController = require("../controllers/PostController");
+const express = require('express')
+const router = express.Router()
+const PostController = require('../controllers/PostController')
 const {
-    authentication,
-    isPostAuthor,
-} = require("../middlewares/authentication");
-const validateBodyParams = require("../middlewares/validateBodyParams");
-const { check } = require("express-validator");
-const { uploadPostImages } = require("../middlewares/upload");
+	authentication,
+	isPostAuthor,
+} = require('../middlewares/authentication')
+const validateBodyParams = require('../middlewares/validateBodyParams')
+const { check } = require('express-validator')
+const { uploadPostImages } = require('../middlewares/upload')
 
-router.get("/getPosts", authentication, PostController.getPosts);
-router.get("/getPostsCreatedByUser/:_id", PostController.getPostsCreatedByUser);
-router.get("/getPostsLikedByUser/:_id", PostController.getPostsLikedByUser);
-router.get("/getPostsQuery", PostController.getPostsQuery);
-router.get("/getAllPosts", PostController.getAllPosts);
-router.get("/getPostById/:_id", PostController.getPostById);
-router.get("/getPostsByTitle/:title", PostController.getPostsByTitle);
+router.get('/getAllPosts', PostController.getAllPosts)
+router.get('/getPosts', authentication, PostController.getPosts)
+router.get('/getPostsCreatedByUser/:_id', PostController.getPostsCreatedByUser)
+router.get('/getPostsLikedByUser/:_id', PostController.getPostsLikedByUser)
+router.get('/getPostsQuery', PostController.getPostsQuery)
+router.get('/getPostById/:_id', PostController.getPostById)
+router.get('/getPostsByTitle/:title', PostController.getPostsByTitle)
 
 router.post(
-    "/",
-    uploadPostImages.single("post_img"),
-    [check("date", "The date cant be empty").notEmpty(), validateBodyParams],
-    authentication,
-    PostController.createPost
-);
+	'/',
+	uploadPostImages.single('post_img'),
+	[check('date', 'The date cant be empty').notEmpty(), validateBodyParams],
+	authentication,
+	PostController.createPost
+)
 
 router.put(
-    "/id/:_id",
-    authentication,
-    isPostAuthor,
-    uploadPostImages.single("post_img"),
-    PostController.updatePost
-);
-router.put("/giveLike/:_id", authentication, PostController.giveLike);
-router.put("/removeLike/:_id", authentication, PostController.removeLike);
+	'/id/:_id',
+	authentication,
+	isPostAuthor,
+	uploadPostImages.single('post_img'),
+	PostController.updatePost
+)
+router.put('/giveLike/:_id', authentication, PostController.giveLike)
+router.put('/removeLike/:_id', authentication, PostController.removeLike)
 
 router.delete(
-    "/id/:_id",
-    authentication,
-    isPostAuthor,
-    PostController.deletePost
-);
+	'/id/:_id',
+	authentication,
+	isPostAuthor,
+	PostController.deletePost
+)
 
-module.exports = router;
+module.exports = router
